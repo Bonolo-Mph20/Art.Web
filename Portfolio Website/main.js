000fb505d0c3e985e6bbacbaeb95843c4139bf42
Bonolo-Mph20 <142890025+Bonolo-Mph20@users.noreply.github.com>
2023-12-22 09:18:51 +0200

    Update main.js

--- Portfolio Website/main.js
+++ Portfolio Website/main.js
@@ -11,3 +11,16 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzx2lpoA9y6bSdaEhC7E9V04PguQyjG882hNNVOXgRdZMzTdB4eHk5p9ZpOJwliehTjhw/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
